213e556ed24d1a1010cf0add31169e78e1132274
David Parra <david.parra@fonyou.com>
2025-03-01 16:49:01 -0500

    Init repository

---
+++ src/test/java/meli/reserva/tickets/reserva_tickets/ReservaTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package meli.reserva.tickets.reserva_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
